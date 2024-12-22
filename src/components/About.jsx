ccc6e9e065dfc787dbc596b18dc3d439a159ed29
Mohamed Tajammul <tajammul183@gmail.com>
2024-12-22 17:18:12 +0530

    implemented about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,23 @@
 import '../App.css'
+import '../styles/About.css'
 
 function About() {
 
   return (
-    <section className="about">
-        
+    <section className="about" id="about">
+        <div className="about-container">
+          <div className="about-container--img">
+            <h1>About Us</h1>
+            <div>
+              <img src='../assets/about_us.png' />
+            </div>
+          </div>
+          <div className="about-container--text">
+            <h2>Why choose us</h2>
+            <h1>Highly experienced <br /> people with us</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab ducimus consequatur dolor est aspernatur quaerat, officia, velit ratione nostrum, commodi doloribus. Temporibus repellat iste quod similique nostrum expedita eum, dolorem quam amet doloribus alias, ducimus nam, dolor voluptate vel! Nisi debitis voluptas, ea sed quae dolorum. Impedit distinctio exercitationem labore!</p>
+          </div>
+        </div>
     </section>
 );
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ function Navbar() {
         hamburgerRef.current.classList.toggle('clicked')
     }
 
-    // const styles = {
-    //     visibility: `${isVisible ? 'visible' : 'hidden'}`,
-    //     transform: `${isVisible ? 'translateX(0vw)' : 'translateX(100vw'}`
-    // }
+    const styles = {
+        visibility: `${isVisible ? 'visible' : 'hidden'}`,
+        transform: `${isVisible ? 'translateX(0vw)' : 'translateX(100vw'}`
+    }
 
   return (
     <nav className="main-nav" id='navbar'>
@@ -27,31 +27,25 @@ function Navbar() {
         </div>
         <div className="main-nav__list">
             <ul>
-                {/* <li><HashLink to='#home'>Home</HashLink></li> */}
                 <li>Home</li>
                 <li>About</li>
                 <li>Services</li>
                 <li>Contact</li>
-
-                {/* <li><HashLink to='#about' smooth>About</HashLink></li>
-                <li><HashLink to='#services' smooth>Services</HashLink></li>
-                <li><HashLink to='#contact' smooth>Contact</HashLink></li> */}
             </ul>
         </div>
-        {/* <div className='hamburger' ref={hamburgerRef} onClick={clickHandler}>
+        <div className='hamburger' ref={hamburgerRef} onClick={clickHandler}>
             <div className='line-1'></div>
             <div className='line-2'></div>
             <div className='line-3'></div>
         </div>
         <div className="hamburger-menu" style={styles}>
             <ul>
-                <li><HashLink to='#home' smooth>Home</HashLink></li>
-                <li><HashLink to='#about' smooth>About</HashLink></li>
-                <li><HashLink to='#services' smooth>Services</HashLink></li>
-                <li><HashLink to='#gallery' smooth>Gallery</HashLink></li>
-                <li><HashLink to='#contact' smooth>Contact</HashLink></li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
             </ul>
-        </div> */}
+        </div>
     </nav>
 );
 
