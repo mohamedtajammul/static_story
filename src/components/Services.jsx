980420d19a6c9b623a20f1679dc88d3714f3536e
Mohamed Tajammul <tajammul183@gmail.com>
2025-01-01 17:38:28 +0530

    finished swiper carousel for services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import '../App.css';
 import '../styles/Services.css'
 
-import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -11,74 +11,52 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 function Services() {
-  useEffect(() => {
-    new Swiper('.swiper', {
-      direction: 'horizontal', // Set to horizontal
-      loop: true,
-      // pagination: {
-      //   el: '.swiper-pagination',
-      //   clickable: true,
-      // },
-      autoplay: {
-        delay: 3000, // 3 seconds delay between slides
-        disableOnInteraction: false, // Continue autoplay after user interactions
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-      },
-      slidesPerView: 1, // Number of visible slides
-      spaceBetween: 10, // Space between slides
-    });
-  }, []);
 
   return (
     <section className="services" id='services'>
       <div className='services-container'>
         <h1>What we do!</h1>
         <h3>Experience the power of innovation.</h3>
-
-        <div className="swiper">
-          <div
-            className="swiper-wrapper"
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <div className="swiper-slide" >
-              <div className='swiper-slide--wrapper'>
-                <div className='slides'><img src="../assets/services11.png" alt="" /></div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi iure hic voluptatibus corrupti esse minus natus, saepe, soluta exercitationem earum accusantium delectus cupiditate quos voluptatem officia, impedit excepturi! Ab, odit.</p>
-              </div>
+         <Swiper
+          modules={[Navigation, Pagination, Scrollbar, Autoplay,  A11y]}
+          spaceBetween={70}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+           autoplay={{
+            delay: 3000, // 3 seconds delay between slides
+            disableOnInteraction: false, // Keep autoplay after user interactions
+          }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+            <div className='swiper-slide--wrapper'>
+              <div className='slides'><img src="../assets/services11.png" alt="" /></div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ut esse ea dignissimos, amet molestias asperiores sed unde molestiae quaerat dolore nesciunt sint eos qui. Harum porro animi impedit fugit. Est natus doloremque officia in, suscipit ab ad corrupti facilis, dolore ullam obcaecati! Vero nemo voluptatem impedit vitae! Reprehenderit numquam est, ipsa explicabo mollitia, eligendi autem porro asperiores impedit aliquam iure odit modi quaerat illum, nesciunt nemo dolor iste ipsum. Necessitatibus provident mollitia eligendi eum tempore pariatur odit aliquam cumque sint! Exercitationem culpa hic perferendis repudiandae nobis sit doloribus esse, alias ratione excepturi sint? Nihil dolore impedit incidunt voluptate deleniti?</p>
             </div>
-            <div className="swiper-slide">
-              <div className='swiper-slide--wrapper'>
-                <div className='slides'><img src="../assets/services12.png" alt="" /></div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi iure hic voluptatibus corrupti esse minus natus, saepe, soluta exercitationem earum accusantium delectus cupiditate quos voluptatem officia, impedit excepturi! Ab, odit.</p>
-              </div>
+          </SwiperSlide>
+           <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+            <div className='swiper-slide--wrapper'>
+              <div className='slides'><img src="../assets/services12.png" alt="" /></div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ut esse ea dignissimos, amet molestias asperiores sed unde molestiae quaerat dolore nesciunt sint eos qui. Harum porro animi impedit fugit. Est natus doloremque officia in, suscipit ab ad corrupti facilis, dolore ullam obcaecati! Vero nemo voluptatem impedit vitae! Reprehenderit numquam est, ipsa explicabo mollitia, eligendi autem porro asperiores impedit aliquam iure odit modi quaerat illum, nesciunt nemo dolor iste ipsum. Necessitatibus provident mollitia eligendi eum tempore pariatur odit aliquam cumque sint! Exercitationem culpa hic perferendis repudiandae nobis sit doloribus esse, alias ratione excepturi sint? Nihil dolore impedit incidunt voluptate deleniti?</p>
             </div>
-            <div className="swiper-slide">
-              <div className='swiper-slide--wrapper'>
-                <div className='slides'><img src="../assets/services13.png" alt="" /></div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi iure hic voluptatibus corrupti esse minus natus, saepe, soluta exercitationem earum accusantium delectus cupiditate quos voluptatem officia, impedit excepturi! Ab, odit.</p>
-              </div>
+          </SwiperSlide>
+           <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+            <div className='swiper-slide--wrapper'>
+              <div className='slides'><img src="../assets/services13.png" alt="" /></div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ut esse ea dignissimos, amet molestias asperiores sed unde molestiae quaerat dolore nesciunt sint eos qui. Harum porro animi impedit fugit. Est natus doloremque officia in, suscipit ab ad corrupti facilis, dolore ullam obcaecati! Vero nemo voluptatem impedit vitae! Reprehenderit numquam est, ipsa explicabo mollitia, eligendi autem porro asperiores impedit aliquam iure odit modi quaerat illum, nesciunt nemo dolor iste ipsum. Necessitatibus provident mollitia eligendi eum tempore pariatur odit aliquam cumque sint! Exercitationem culpa hic perferendis repudiandae nobis sit doloribus esse, alias ratione excepturi sint? Nihil dolore impedit incidunt voluptate deleniti?</p>
             </div>
-          </div>
-
-          {/* Pagination dots */}
-          <div className="swiper-pagination"></div>
-          {/* Navigation arrows */}
-          <div className="swiper-button-prev"></div>
-          <div className="swiper-button-next"></div>
-          {/* Scrollbar */}
-          <div className="swiper-scrollbar"></div>
-        </div>
+          </SwiperSlide>
+          <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+            <div className='swiper-slide--wrapper'>
+              <div className='slides'><img src="../assets/services13.png" alt="" /></div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ut esse ea dignissimos, amet molestias asperiores sed unde molestiae quaerat dolore nesciunt sint eos qui. Harum porro animi impedit fugit. Est natus doloremque officia in, suscipit ab ad corrupti facilis, dolore ullam obcaecati! Vero nemo voluptatem impedit vitae! Reprehenderit numquam est, ipsa explicabo mollitia, eligendi autem porro asperiores impedit aliquam iure odit modi quaerat illum, nesciunt nemo dolor iste ipsum. Necessitatibus provident mollitia eligendi eum tempore pariatur odit aliquam cumque sint! Exercitationem culpa hic perferendis repudiandae nobis sit doloribus esse, alias ratione excepturi sint? Nihil dolore impedit incidunt voluptate deleniti?</p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+       
       </div>
 
     </section>
